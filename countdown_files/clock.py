ca41bc9291504a0a2a6f47da775b75e4669b3e81
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2019-07-20 11:22:37 -0400

    Correction
    It's actually 108 minutes not 180
    
    Lost is my favourite TV show 😉

--- countdown_files/clock.py
+++ countdown_files/clock.py
@@ -4,11 +4,11 @@ import os
 os.system('cls')
 
 def countdown():
-	countdown_time=181
+	countdown_time=6481
 	while countdown_time >0:
 		print(countdown_time - 1)
 		time.sleep(1)
-		#time.sleep(60) #uncomment for 180 mins rather than 180 seconds
+		time.sleep(60) #comment for 108 secs rather than 108 mins
 		countdown_time -=1
 	os.system('rundll32.exe user32.dll,LockWorkStation')
 	os.system('taskkill /f /IM python.exe')
