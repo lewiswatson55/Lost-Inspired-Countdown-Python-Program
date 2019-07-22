c19272a1a756495a38e0bd83d4e1a993226906d2
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2019-07-22 19:54:35 -0400

    smaller filesize (0.1.1)
    possible bugfix, too

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 echo off
-echo start.bat forked version 0.1
-start py countdown_files/clock.py
-start py countdown_files/countdown.py
+echo start.bat forked version 0.1.1
+py countdown_files/clock.py
+py countdown_files/countdown.py
 exit
