fdd001f04f90f1d455cb39c8c18d5ad4749ac175
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2019-07-23 15:52:24 -0400

    added a feature
    0.1
    who are you—locke?

--- countdown_files/countdown.py
+++ countdown_files/countdown.py
@@ -1,3 +1,4 @@
+print("Forked v.0.1")
 #Lost Inspired Countdown
 #Lewis N Watson
 
@@ -7,13 +8,24 @@ import os
 
 def start_countdown():
 	os.system('cls')
-	u_input = input("_> ")
+	u_input = input(">: ")
 
 	if str(u_input) == "4815162342":
 		print("Timer Resetting...")
 		time.sleep(2)
 		os.system('start start.bat')
 		os.system('taskkill /f /IM python.exe')
+	elif str(u_input) == "4 8 15 16 23 42":
+		print("Timer Resetting...")
+		time.sleep(2)
+		os.system('start start.bat')
+		os.system('taskkill /f /IM python.exe')
+		
+	elif str(u_input) == "4 8 15 16 23 32":
+		print("Who are you — Locke? ")
+		
+	elif str(u_input) == "4815162332":
+		print("Who are you — Locke? ")
 
 	else:
 		os.system('cls')
