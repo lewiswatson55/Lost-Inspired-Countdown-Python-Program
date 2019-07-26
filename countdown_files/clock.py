cce920790dcc570e6c058c691e962eaff4cbd573
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2019-07-26 10:12:19 -0400

    Update clock.py
    • bumped version
    • added version
    • mac version added

--- countdown_files/clock.py
+++ countdown_files/clock.py
@@ -10,7 +10,18 @@ def countdown():
 		time.sleep(1)
 		time.sleep(60) #comment for 108 secs rather than 108 mins
 		countdown_time -=1
-	os.system('rundll32.exe user32.dll,LockWorkStation')
-	os.system('taskkill /f /IM python.exe')
+	win = input("Is your OS Windows? (y/n, case-sensitive)")
+	if win == "y":
+		os.system('rundll32.exe user32.dll,LockWorkStation')
+		os.system('taskkill /f /IM python.exe')
+	else: 
+		mac = input("Mac? (y/n, case-sensitive)")
+		if mac == "y":
+			os.system('/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend')
+			os.system('killall python')
+		else: 
+			print("Then I assume Linux")
+			print("Under Construction")
 
 countdown();
+print("Forked version 0.2, thanks for using")
