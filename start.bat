40c8e8615db53ca6ea2776cce79ba3dfa29ef63a
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2019-09-03 13:32:56 -0400

    Update start.bat
    BUGFIX

--- start.bat
+++ start.bat
@@ -1,5 +1,4 @@
 echo off
-echo start.bat forked version 0.1.2
-start python countdown_files/clock.py
-start python countdown_files/countdown.py
+start python countdown_files\clock.py
+start python countdown_files\countdown.py
 exit
